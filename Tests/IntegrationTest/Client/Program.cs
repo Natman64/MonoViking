9037801cce8c6dd82723b76075b8bc1ddc63e6cf
Nathaniel Nelson <nelson.nleroy@gmail.com>
2015-07-07 15:42:59 -0600

    Multi-window integration test

---
+++ Tests/IntegrationTest/Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MonogameTest;
+using Gtk;
+using System.Threading;
+
+namespace Client
+{
+	public class Program
+	{
+		public static void Main (string[] args)
+		{
+
+
+			Game1 game = new Game1 ();
+
+			Thread thread = new Thread ( () => { game.Run(); });
+			thread.Start ();
+
+			for (int i = 0; i < 100000; i++) {
+				for (int j = 0; j < 100; j++) {
+				}
+			}
+
+			Application.Init ();
+			MainWindow window = new MainWindow ();
+			window.Game = game;
+
+			window.Show ();
+			Application.Run ();
+		}
+	}
+}
+
---
+++ Tests/IntegrationTest/GTKClient/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using MonogameTest;
+
+public partial class MainWindow: Gtk.Window
+{
+	public Game1 Game {
+		get;
+		set;
+	}
+
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton1Clicked (object sender, EventArgs e)
+	{
+		if (Game != null) {
+			Game.Swap ();
+		}
+	}
+}
